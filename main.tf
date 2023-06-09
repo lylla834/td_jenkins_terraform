10d1783383247b735e8181d69d8c617f3fc68b63
lylla834 <123461723+lylla834@users.noreply.github.com>
2023-06-09 10:40:52 +0200

    Add files via upload

--- main.tf
+++ main.tf
@@ -21,130 +21,61 @@ provider "azurerm" {
 
 
 
-resource "azurerm_resource_group" "webserver" {
-   name = "terraform_jenkins_lylla"
-   location = var.location
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
 }
 
-resource "azurerm_network_security_group" "allowedports" {
-   name = "allowedports"
-   resource_group_name = azurerm_resource_group.webserver.name
-   location = azurerm_resource_group.webserver.location
-  
-   security_rule {
-       name = "http"
-       priority = 100
-       direction = "Inbound"
-       access = "Allow"
-       protocol = "Tcp"
-       source_port_range = "*"
-       destination_port_range = "80"
-       source_address_prefix = "*"
-       destination_address_prefix = "*"
-   }
-
-   security_rule {
-       name = "https"
-       priority = 200
-       direction = "Inbound"
-       access = "Allow"
-       protocol = "Tcp"
-       source_port_range = "*"
-       destination_port_range = "443"
-       source_address_prefix = "*"
-       destination_address_prefix = "*"
-   }
-
-   security_rule {
-       name = "ssh"
-       priority = 300
-       direction = "Inbound"
-       access = "Allow"
-       protocol = "Tcp"
-       source_port_range = "*"
-       destination_port_range = "22"
-       source_address_prefix = "*"
-       destination_address_prefix = "*"
-   }
-}
-
-resource "azurerm_public_ip" "webserver_public_ip" {
-   name = "webserver_public_ip"
-   location = var.location
-   resource_group_name = azurerm_resource_group.webserver.name
-   allocation_method = "Dynamic"
-
-   tags = {
-       environment = var.environment
-       costcenter = "it"
-   }
-
-   depends_on = [azurerm_resource_group.webserver]
-}
-
-resource "azurerm_network_interface" "webserver" {
-   name = "nginx-interface"
-   location = azurerm_resource_group.webserver.location
-   resource_group_name = azurerm_resource_group.webserver.name
-
-   ip_configuration {
-       name = "internal"
-       private_ip_address_allocation = "Dynamic"
-       subnet_id = azurerm_subnet.webserver-subnet.id
-       public_ip_address_id = azurerm_public_ip.webserver_public_ip.id
-   }
-
-   depends_on = [azurerm_resource_group.webserver]
-}
-
-resource "azurerm_linux_virtual_machine" "nginx" {
-   size = var.instance_size
-   name = "nginx-webserver"
-   resource_group_name = azurerm_resource_group.webserver.name
-   location = azurerm_resource_group.webserver.location
-   network_interface_ids = [
-       azurerm_network_interface.webserver.id,
-   ]
-
-   source_image_reference {
-       publisher = "Canonical"
-       offer = "UbuntuServer"
-       sku = "18.04-LTS"
-       version = "latest"
-   }
-
-   computer_name = "vm_terra_jenkins"
-   admin_username = "lylla"
-   admin_password = "Jevis@ambronay"
-   disable_password_authentication = false
-
-   os_disk {
-       name = "nginxdisk01"
-       caching = "ReadWrite"
-       #create_option = "FromImage"
-       storage_account_type = "Standard_LRS"
-   }
-
-   tags = {
-       environment = var.environment
-       costcenter = "it"
-   }
-
-   depends_on = [azurerm_resource_group.webserver]
-}
-resource "azurerm_virtual_network" "webserver-net" {
-  name                = "webserver-net"
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.webserver.location
-  resource_group_name = azurerm_resource_group.webserver.name
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
 }
 
-resource "azurerm_subnet" "webserver-subnet" {
-  name                 = "subnet01"
-  resource_group_name  = azurerm_resource_group.webserver.name
-  virtual_network_name = azurerm_virtual_network.webserver-net.name
-  address_prefixes       = ["10.0.1.0/24"]
-
-  private_link_service_network_policies_enabled = false
+resource "azurerm_subnet" "example" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.2.0/24"]
 }
 
+resource "azurerm_network_interface" "example" {
+  name                = "example-nic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "example" {
+  name                = "example-machine"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "20.04-LTS"
+    version   = "latest"
+  }
+}
