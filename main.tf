12cddc3a744f34dba150033aa7680447817d757b
lylla834 <123461723+lylla834@users.noreply.github.com>
2023-06-09 12:48:09 +0200

    Add files via upload

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ provider "azurerm" {
 
 resource "azurerm_resource_group" "webserver" {
    name = "terraform_jenkins_lylla"
-   location = "West Europe"
+   location = var.location
 }
 
 resource "azurerm_network_security_group" "allowedports" {
@@ -126,6 +126,43 @@ resource "azurerm_linux_virtual_machine" "nginx" {
        storage_account_type = "Standard_LRS"
    }
 
+variable "subscription_id" {
+   description = "Azure subscription"
+   default = "393e3de3-0900-4b72-8f1b-fb3b1d6b97f1"
+}
+
+#variable "client_id" {
+#   description = "Azure Client ID"
+#   default = "384ba4e4-fc1a-47f4-bf42-acec6fad1e82"
+#}
+
+#variable "client_secret" {
+#   description = "Azure Client Secret"
+#   default = "lxk8Q~LfBuObGY4frVfc6B_e4Nqf6UMahS-BdaD-"
+#}
+
+variable "tenant_id" {
+   description = "Azure Tenant ID"
+   default = "7349d3b2-951f-41be-877e-d8ccd9f3e73c"
+}
+
+variable "instance_size" {
+   type = string
+   description = "Azure instance size"
+   default = "Standard_F2"
+}
+
+variable "location" {
+   type = string
+   description = "Region"
+   default = "West Europe"
+}
+
+variable "environment" {
+   type = string
+   description = "Environment"
+   default = "dev"
+}
    
    depends_on = [azurerm_resource_group.webserver]
 }
