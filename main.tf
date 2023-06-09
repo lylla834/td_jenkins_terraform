f260c72a1226bb551224cae1508354e0b0554702
lylla834 <123461723+lylla834@users.noreply.github.com>
2023-06-09 12:31:53 +0200

    Add files via upload

--- main.tf
+++ main.tf
@@ -21,58 +21,111 @@ provider "azurerm" {
 
 
 
-resource "azurerm_resource_group" "example" {
-  name     = "example-resourceslylla"
-  location = "West Europe"
+resource "azurerm_resource_group" "webserver" {
+   name = "terraform_jenkins_lylla"
+   location = var.location
 }
 
-resource "azurerm_virtual_network" "example" {
-  name                = "example-network"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+resource "azurerm_network_security_group" "allowedports" {
+   name = "allowedports"
+   resource_group_name = azurerm_resource_group.webserver.name
+   location = azurerm_resource_group.webserver.location
+  
+   security_rule {
+       name = "http"
+       priority = 100
+       direction = "Inbound"
+       access = "Allow"
+       protocol = "Tcp"
+       source_port_range = "*"
+       destination_port_range = "81"
+       source_address_prefix = "*"
+       destination_address_prefix = "*"
+   }
+
+   security_rule {
+       name = "https"
+       priority = 200
+       direction = "Inbound"
+       access = "Allow"
+       protocol = "Tcp"
+       source_port_range = "*"
+       destination_port_range = "443"
+       source_address_prefix = "*"
+       destination_address_prefix = "*"
+   }
+
+   security_rule {
+       name = "ssh"
+       priority = 300
+       direction = "Inbound"
+       access = "Allow"
+       protocol = "Tcp"
+       source_port_range = "*"
+       destination_port_range = "22"
+       source_address_prefix = "*"
+       destination_address_prefix = "*"
+   }
 }
 
-resource "azurerm_subnet" "example" {
-  name                 = "internal"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["10.0.2.0/24"]
+resource "azurerm_public_ip" "webserver_public_ip" {
+   name = "webserver_public_ip"
+   location = var.location
+   resource_group_name = azurerm_resource_group.webserver.name
+   allocation_method = "Dynamic"
+
+   tags = {
+       environment = var.environment
+       costcenter = "it"
+   }
+
+   depends_on = [azurerm_resource_group.webserver]
 }
 
-resource "azurerm_network_interface" "example" {
-  name                = "example-nic"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+resource "azurerm_network_interface" "webserver" {
+   name = "nginx-interface"
+   location = azurerm_resource_group.webserver.location
+   resource_group_name = azurerm_resource_group.webserver.name
 
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.example.id
-    private_ip_address_allocation = "Dynamic"
-  }
+   ip_configuration {
+       name = "internal"
+       private_ip_address_allocation = "Dynamic"
+       subnet_id = azurerm_subnet.webserver-subnet.id
+       public_ip_address_id = azurerm_public_ip.webserver_public_ip.id
+   }
+
+   depends_on = [azurerm_resource_group.webserver]
 }
 
-resource "azurerm_linux_virtual_machine" "example" {
-  name                = "example-machine"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-  size                = "Standard_F2"
-  admin_username      = "lylla"
-  admin_password      = "Jevis@ambronay"
-  disable_password_authentication = false
-  network_interface_ids = [
-    azurerm_network_interface.example.id,
-  ]
+resource "azurerm_linux_virtual_machine" "nginx" {
+   size = var.instance_size
+   name = "nginx-webserver"
+   resource_group_name = azurerm_resource_group.webserver.name
+   location = azurerm_resource_group.webserver.location
+   custom_data = base64encode(file("init-script.sh"))
+   network_interface_ids = [
+       azurerm_network_interface.webserver.id,
+   ]
 
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
+   source_image_reference {
+       publisher = "Canonical"
+       offer = "UbuntuServer"
+       sku = "18.04-LTS"
+       version = "latest"
+   }
 
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-focal"
-    sku       = "20_04-lts"
-    version   = "latest"
-  }
+   computer_name = "nginx"
+   admin_username = "lylla"
+   admin_password = "Jevis@ambronay"
+   disable_password_authentication = false
+
+   os_disk {
+       name = "nginxdisk01"
+       caching = "ReadWrite"
+       #create_option = "FromImage"
+       storage_account_type = "Standard_LRS"
+   }
+
+   
+   depends_on = [azurerm_resource_group.webserver]
 }
